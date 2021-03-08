6be6ed34abfa283a4a772fccabfe5099e537c6c2
TNQ <tnq@elca.vn>
2021-03-08 12:33:59 +0700

    Update FAM logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Hello, Fam</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main onClick={handleClick} className={styles.main}>
         <h1 className={styles.title}>
-          Hello, fam!
+          Hello, Fam!
         </h1>
       </main>
     </div>
